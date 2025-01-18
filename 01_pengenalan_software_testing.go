cfba71c62e4897d61d0ce1d70d1f54f74c4aec06
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2025-01-19 04:36:22 +0800

    pengenalan software testing

---
+++ 01_pengenalan_software_testing.go
@@ -0,0 +1,13 @@
+package main
+
+/*
+Pengenalan Software Testing
+- Software testing adalah salah satu disiplin ilmu dalam software engineering
+- Tujuan utama dari software testing adalah memastikan kualitas kode dan aplikasi kita baik
+- Ilmu untuk software testing sendiri sangatlah luas, pada materi ini kita hanya akan fokus ke unit testing
+
+Unit Test
+- Unit test akan fokus menguji bagian kode program terkecil, biasanya menguji sebuah method
+- Unit test biasnya dibuat kecil dan cepat, oleh karena itu biasnya kadang kode unit test lebih banyak dari kode program aslinya, karena semua skenario pengujian akan dicoba di unit test
+- Unit test bisa digunakan sebagai cara untuk meningkatkan kualitas kode program kita
+*/
